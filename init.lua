37970ed1bdb1710203ad11059c78ba0613357ca0
Ingrid <git@ingrids.email>
2021-12-09 22:27:42 +0100

    init: document disabled distribution plugins

--- init.lua
+++ init.lua
@@ -12,20 +12,37 @@ local skip_providers = function()
 end
 
 local disable_distribution_plugins = function()
+    -- edit inside zips
     g.loaded_gzip              = 1
     g.loaded_tar               = 1
     g.loaded_tarPlugin         = 1
     g.loaded_zip               = 1
     g.loaded_zipPlugin         = 1
+
+    -- update vimscripts. Seems redundant with plugin managers
     g.loaded_getscript         = 1
     g.loaded_getscriptPlugin   = 1
+
+    -- vim's own archiving system???
     g.loaded_vimball           = 1
     g.loaded_vimballPlugin     = 1
+
+    -- extends automatic parenthesis navigation (%) to also work with keywords
     g.loaded_matchit           = 1
+
+    -- highlight parenthesis matching that under the cursor
     g.loaded_matchparen        = 1
+
+    -- convert buffer to html?
     g.loaded_2html_plugin      = 1
+
+    -- apply logic to regex searches (:LogiPat)
     g.loaded_logiPat           = 1
+
+    -- utility to improve remote-vim experience?
     g.loaded_rrhelper          = 1
+
+    -- builtin file explorer
     g.loaded_netrwPlugin       = 1
     g.loaded_netrwSettings     = 1
     g.loaded_netrwFileHandlers = 1
