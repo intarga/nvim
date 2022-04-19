d860c10bddd76787a3ff6f7a86c4bf7bca0e9f16
Ingrid <git@ingrids.email>
2022-04-19 20:26:22 +0200

    move plugin configs to plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -24,24 +24,201 @@ return require('packer').startup(function()
     -- use 'guns/vim-sexp'
 
     -- comment management
-    use 'b3nj5m1n/kommentary'
+    use {
+        'b3nj5m1n/kommentary',
+        config = function()
+            require('kommentary.config').configure_language("default", {
+                prefer_single_line_comments = true,
+            })
+        end
+    }
+
 
     -- search tools
-    use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} },
+        config = function()
+            local telescope = require('telescope')
+            telescope.setup {
+                defaults = require('telescope.themes').get_dropdown {
+                    mappings = {
+                        i = { ["<C-B>"] = "select_default" }
+                    }
+                },
+                extensions = {
+                    fzf = {
+                        fuzzy = true, -- false will only do exact matching
+                        override_generic_sorter = true,
+                        override_file_sorter = true,
+                        case_mode = "smart_case",
+                    }
+                }
+            }
+            telescope.load_extension('fzf')
+        end
+    }
     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
     -- status line
-    use 'nvim-lualine/lualine.nvim'
+    use {
+        'nvim-lualine/lualine.nvim',
+        config = function()
+            local my16color = {
+                normal = {
+                    a = { fg = 0, bg = 12, gui = 'bold' },
+                    b = { fg = 0, bg = 4 },
+                    c = { fg = NONE, bg = 0 },
+                },
+                insert = {
+                    a = { fg = 0, bg = 13, gui = 'bold' },
+                    b = { fg = 0, bg = 5 },
+                },
+                visual = {
+                    a = { fg = 0, bg = 10, gui = 'bold' },
+                    b = { fg = 0, bg = 2 },
+                },
+                replace = {
+                    a = { fg = 0, bg = 11, gui = 'bold' },
+                    b = { fg = 0, bg = 3 },
+                },
+            }
+            require'lualine'.setup {
+                options = {
+                    theme = my16color,
+                    component_separators = { left = '', right = ''},
+                    section_separators = { left = '', right = ''},
+                    always_divide_middle = true,
+                },
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch'},
+                    lualine_c = {{'buffers', buffers_color = {
+                        active = {fg = NONE},
+                        inactive = {fg = 12},
+                    }}},
+                    lualine_x = {
+                        {'diff', diff_color = {
+                            added = {fg = 2},
+                            modified = {fg = 3},
+                            removed = {fg = 5},
+                        }},
+                        {'diagnostics', icons_enabled = false, diagnostics_color = {
+                            error = {fg = 0, bg = 9},
+                            warn = {fg = 1},
+                            info = {fg = 3},
+                            hint = {fg = 4},
+                        }},
+                    },
+                    lualine_y = {
+                        'encoding',
+                        {'filetype', color = {gui = 'bold'}},
+                        {'fileformat', icons_enabled = false},
+                    },
+                    lualine_z = {'progress', 'location'}
+                },
+            }
+        end
+    }
 
     -- lisp repl integration
-    use 'Olical/conjure'
+    use {
+        'Olical/conjure',
+        config = function()
+            vim.g['conjure#log#hud#anchor'] = "SE"
+            vim.g['conjure#log#hud#border'] = "none"
+            vim.g['conjure#eval#inline#prefix'] = "-> "
+            vim.g['conjure#log#hud#width'] = 1.0
+            vim.g['conjure#log#hud#enabled'] = false
+            vim.g['conjure#mapping#eval_current_form'] = "f"
+            vim.g['conjure#mapping#eval_comment_current_form'] = "c"
+        end,
+        ft = 'scheme'
+    }
 
     -- syntax highlighting
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+                ensure_installed = "all",
+                highlight = { enable = true },
+                -- indent = { enable = true },
+            }
+        end
+    }
 
     -- language server
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/nvim-compe'
+    use {
+        'neovim/nvim-lspconfig',
+        config = function()
+            -- LSP
+            local lspconfig = require'lspconfig'
+            lspconfig.gopls.setup{}
+            lspconfig.rust_analyzer.setup{}
+            lspconfig.pyright.setup{}
+            lspconfig.efm.setup {
+                init_options = {documentFormatting = true},
+                filetypes = { 'lua','sh','python','css','html' },
+                settings = {
+                    rootMarkers = {".git/"},
+                    languages = {
+                        lua = {
+                            {formatCommand = "lua-format -i", formatStdin = true}
+                        },
+                        sh = {
+                            {
+                                lintCommand = "shellcheck -f gcc -x",
+                                lintSource = "shellcheck",
+                                lintFormats = {
+                                    "%f:%l:%c: %trror: %m",
+                                    "%f:%l:%c: %tarning: %m",
+                                    "%f:%l:%c: %tote: %m"
+                                }
+                            },
+                            {formatCommand = "shfmt -ci -s -bn", formatStdin = true}
+                        },
+                        python = {
+                            {formatCommand = "black --quiet -", formatStdin = true}
+                        },
+                        css = {
+                            {formatCommand = "prettier --parser css", formatStdin = true}
+                        },
+                        html = {
+                            {formatCommand = "prettier --parser html", formatStdin = true}
+                        },
+                    }
+                }
+            }
+        end
+    }
+
+    -- autocompletion
+    use {
+        'hrsh7th/nvim-compe',
+        config = function()
+            require'compe'.setup {
+                enabled = true;
+                autocomplete = true;
+                debug = false;
+                min_length = 1;
+                preselect = 'disable';
+                throttle_time = 80;
+                source_timeout = 200;
+                incomplete_delay = 400;
+                allow_prefix_unmatch = false;
+                source = {
+                    path = true;
+                    buffer = true;
+                    vsnip = true;
+                    nvim_lsp = true;
+                    nvim_lua = true;
+                    your_awesome_source = {};
+                };
+            }
+        end
+    }
     --use 'hrsh7th/vim-vsnip'
     --use 'hrsh7th/vim-vsnip-integ'
 end)
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,34 +1,31 @@
-local cmd, o, b, w, au = vim.api.nvim_command, vim.o, vim.bo, vim.wo, vim.api.nvim_create_autocmd
+local cmd, au, opt = vim.api.nvim_command, vim.api.nvim_create_autocmd, vim.opt
 local settings = {}
 
 function settings.setup()
     -- tabs vs spaces
     local indent = 4
-    b.expandtab = true
-    o.expandtab = true -- might become unnecessary?
-    b.shiftwidth = indent
-    o.shiftwidth = indent
-    b.tabstop = indent
-    o.tabstop = indent
+    opt.expandtab = true
+    opt.shiftwidth = indent
+    opt.tabstop = indent
 
     -- enable mouse in all modes
-    o.mouse = 'a'
+    opt.mouse = 'a'
 
     -- use system clipboard - may require installing xclip
-    o.clipboard = 'unnamedplus'
+    opt.clipboard = 'unnamedplus'
 
     -- open splits below and to the right
-    o.splitbelow = true
-    o.splitright = true
+    opt.splitbelow = true
+    opt.splitright = true
 
     -- line numbers
-    w.number = true
-    w.relativenumber = true
-    w.cursorline = true
-    w.cursorlineopt = "number"
+    opt.number = true
+    opt.relativenumber = true
+    opt.cursorline = true
+    opt.cursorlineopt = "number"
 
     -- enable autocompletion
-    o.completeopt = 'menu,menuone,noselect'
+    opt.completeopt = 'menu,menuone,noselect'
 
     -- colours!
     -- redo when api is fixed...
@@ -64,167 +61,6 @@ function settings.setup()
     -- disable matchparen in insert mode
     au("InsertEnter", { pattern = "*", command = "NoMatchParen" })
     au("InsertLeave", { pattern = "*", command = "DoMatchParen" })
-
-    -- conjure
-    vim.g['conjure#log#hud#anchor'] = "SE"
-    vim.g['conjure#log#hud#border'] = "none"
-    vim.g['conjure#eval#inline#prefix'] = "-> "
-    vim.g['conjure#log#hud#width'] = 1.0
-    vim.g['conjure#log#hud#enabled'] = false
-    vim.g['conjure#mapping#eval_current_form'] = "f"
-    vim.g['conjure#mapping#eval_comment_current_form'] = "c"
-
-    -- Kommentary
-    --vim.g.kommentary_create_default_mappings = false
-    --require('kommentary.config').use_extended_mappings()
-    require('kommentary.config').configure_language("default", {
-        prefer_single_line_comments = true,
-    })
-
-    -- telescope
-    local telescope = require('telescope')
-    telescope.setup {
-        defaults = require('telescope.themes').get_dropdown {
-            mappings = {
-                i = {
-                    ["<C-B>"] = "select_default",
-                }
-            }
-        },
-        extensions = {
-            fzf = {
-                fuzzy = true, -- false will only do exact matching
-                override_generic_sorter = true,
-                override_file_sorter = true,
-                case_mode = "smart_case",
-            }
-        }
-    }
-    telescope.load_extension('fzf')
-
-    -- status line
-    local my16color = {
-        normal = {
-            a = { fg = 0, bg = 12, gui = 'bold' },
-            b = { fg = 0, bg = 4 },
-            c = { fg = NONE, bg = 0 },
-        },
-        insert = {
-            a = { fg = 0, bg = 13, gui = 'bold' },
-            b = { fg = 0, bg = 5 },
-        },
-        visual = {
-            a = { fg = 0, bg = 10, gui = 'bold' },
-            b = { fg = 0, bg = 2 },
-        },
-        replace = {
-            a = { fg = 0, bg = 11, gui = 'bold' },
-            b = { fg = 0, bg = 3 },
-        },
-    }
-
-    require'lualine'.setup {
-        options = {
-            theme = my16color,
-            component_separators = { left = '', right = ''},
-            section_separators = { left = '', right = ''},
-            always_divide_middle = true,
-        },
-        sections = {
-            lualine_a = {'mode'},
-            lualine_b = {'branch'},
-            lualine_c = {{'buffers', buffers_color = {
-                active = {fg = NONE},
-                inactive = {fg = 12},
-            }}},
-            lualine_x = {
-                {'diff', diff_color = {
-                    added = {fg = 2},
-                    modified = {fg = 3},
-                    removed = {fg = 5},
-                }},
-                {'diagnostics', icons_enabled = false, diagnostics_color = {
-                    error = {fg = 0, bg = 9},
-                    warn = {fg = 1},
-                    info = {fg = 3},
-                    hint = {fg = 4},
-                }},
-            },
-            lualine_y = {
-                'encoding',
-                {'filetype', color = {gui = 'bold'}},
-                {'fileformat', icons_enabled = false},
-            },
-            lualine_z = {'progress', 'location'}
-        },
-    }
-
-    -- Treesitter
-    require'nvim-treesitter.configs'.setup {
-        ensure_installed = "all",
-        highlight = { enable = true },
-        -- indent = { enable = true },
-    }
-
-    -- LSP
-    local lspconfig = require'lspconfig'
-    lspconfig.gopls.setup{}
-    lspconfig.rust_analyzer.setup{}
-    lspconfig.pyright.setup{}
-    lspconfig.efm.setup {
-        init_options = {documentFormatting = true},
-        filetypes = { 'lua','sh','python','css','html' },
-        settings = {
-            rootMarkers = {".git/"},
-            languages = {
-                lua = {
-                    {formatCommand = "lua-format -i", formatStdin = true}
-                },
-                sh = {
-                    {
-                        lintCommand = "shellcheck -f gcc -x",
-                        lintSource = "shellcheck",
-                        lintFormats = {
-                            "%f:%l:%c: %trror: %m",
-                            "%f:%l:%c: %tarning: %m",
-                            "%f:%l:%c: %tote: %m"
-                        }
-                    },
-                    {formatCommand = "shfmt -ci -s -bn", formatStdin = true}
-                },
-                python = {
-                    {formatCommand = "black --quiet -", formatStdin = true}
-                },
-                css = {
-                    {formatCommand = "prettier --parser css", formatStdin = true}
-                },
-                html = {
-                    {formatCommand = "prettier --parser html", formatStdin = true}
-                },
-            }
-        }
-    }
-
-    require'compe'.setup {
-        enabled = true;
-        autocomplete = true;
-        debug = false;
-        min_length = 1;
-        preselect = 'disable';
-        throttle_time = 80;
-        source_timeout = 200;
-        incomplete_delay = 400;
-        allow_prefix_unmatch = false;
-
-        source = {
-            path = true;
-            buffer = true;
-            vsnip = true;
-            nvim_lsp = true;
-            nvim_lua = true;
-            your_awesome_source = {};
-        };
-    }
 end
 
 return settings
