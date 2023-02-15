3027ad930cfe9983c4e55e670ee6de38b38f5426
Ingrid <git@ingrids.email>
2023-02-15 13:16:56 +0100

    plugins: tab mappings for cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -232,9 +232,27 @@ return require('lazy').setup({
                 mapping = cmp.mapping.preset.insert({
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
                     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ['<Tab>'] = function(fallback)
+                        if not cmp.select_next_item() then
+                            if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                                cmp.complete()
+                            else
+                                fallback()
+                            end
+                        end
+                    end,
+
+                    ['<S-Tab>'] = function(fallback)
+                        if not cmp.select_prev_item() then
+                            if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                                cmp.complete()
+                            else
+                                fallback()
+                            end
+                        end
+                    end,
                 }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
