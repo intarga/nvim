650017b3cdab717b616ea08feca405c50572a7a4
Ingrid <git@ingrids.email>
2023-03-26 21:09:17 +0200

    plugins: fix panicking tab on start of line
    function has_words_before was missing

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -227,6 +227,12 @@ return require('lazy').setup({
     {
         'hrsh7th/nvim-cmp',
         config = function()
+            local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
+
             local cmp = require'cmp'
 
             cmp.setup({
